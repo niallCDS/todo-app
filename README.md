1c2a685c7329693a424214f8cce3b3e58db4b39d
Niall Gillmor <niall@gillmor.org>
2020-12-01 19:32:30 +0800

    Prettier and cleaned up code

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Simple TODO web app for CFC.
+
 ## Instructions
+
 - Enter to add new task.
 - Left click on button to complete/uncomplete task.
 - Shift + left click on botton to delete task.
--- main.js
+++ main.js
@@ -1,50 +1,47 @@
-const form = document.getElementById("taskForm");
-const list = document.getElementById("tasks");
+const form = document.getElementById('taskForm');
+const list = document.getElementById('tasks');
 
-form.addEventListener("submit", addTask);
-
-let taskCount = 0;
+form.addEventListener('submit', addTask);
 
 function addTask(e) {
-    e.preventDefault();
-    
-    let task = document.getElementById("task-input").value;
-    if (task && taskCount < 8) {
-        taskCount++;
+	e.preventDefault();
 
-        let newTask = document.createElement("li");
-        newTask.className = "task";
-        let newTaskTitle = document.createTextNode(task);
-        let button = document.createElement("button");
-        button.className = "button";
-        newTask.appendChild(button);
-        newTask.appendChild(newTaskTitle);
+	const task = document.getElementById('task-input').value;
+	if (task) {
+		const newTask = document.createElement('li');
+		newTask.className = 'task';
 
-        list.appendChild(newTask);
-        form.reset();
-    } else {
-        alert("Invalid task.")
-    }
-    
+		const newTaskTitle = document.createTextNode(task);
+		const button = document.createElement('button');
+		button.className = 'button';
+
+		newTask.appendChild(button);
+		newTask.appendChild(newTaskTitle);
+
+		list.appendChild(newTask);
+		form.reset();
+	} else {
+		alert('Invalid task.');
+	}
 }
 
-tasks.addEventListener("click", modifyTask)
-function modifyTask(e) {
-    if (e.target.tagName == "BUTTON") {
-        let task = e.target.parentElement;
-        // Complete/uncomplete task.
-        if (task.className == "task"){
-            e.target.className = "button completed-button";
-            task.className = "task completed-task";
-        } else if (task.className = "task completed-task"){
-            e.target.className = "button";
-            task.className = "task";
-        }
-        // Delete task.
-        if (e.shiftKey) {
-            task.remove();
-            taskCount--;
-        }
+tasks.addEventListener('click', modifyTask);
 
-    }
+function modifyTask(e) {
+	if (e.target.tagName == 'BUTTON') {
+		const task = e.target.parentElement;
+
+		// Complete/uncomplete task.
+		if (task.className == 'task') {
+			e.target.className = 'button completed-button';
+			task.className = 'task completed-task';
+		} else if ((task.className = 'task completed-task')) {
+			e.target.className = 'button';
+			task.className = 'task';
+		}
+		// Delete task.
+		if (e.shiftKey) {
+			task.remove();
+		}
+	}
 }
